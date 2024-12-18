7bd9b6dc32784b685214fc9a1142f9304f85b803
Владислав Рыжов <vladryzov@gmail.cmo>
2024-12-19 00:51:08 +0300

    feat: Добавлено описание в рамках ДЗ

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          1. В рамках курса хочу овладеть компетенциями в фронтенд разработке 2. React 3. Частично знаком с jQuery и
+          небольшой опыта в верстке 4. 4 года оптыа backend на Java
         </p>
       </header>
     </div>
